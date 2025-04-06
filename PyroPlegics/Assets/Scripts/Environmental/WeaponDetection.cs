8a297c6ea41728f048166d6e8e7799eac63419f1
KAPsLuck <kpine@uwo.ca>
2025-04-06 19:48:56 -0400

    Created WeaponPickup GameObjects for Levels 2 and 3
    Acts like a chest that the Player interacts with to learn that they unlocked a new weapon that they can use for the next level.

---
+++ PyroPlegics/Assets/Scripts/Environmental/WeaponDetection.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class WeaponDetection : MonoBehaviour
+{
+    public GameObject dialogueTemplate;
+    public GameObject canva;
+    public GameObject canvaInteract;
+    bool playerInRange = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange)
+        {
+            canvaInteract.SetActive(true);
+        }
+        else {canvaInteract.SetActive(false);}
+        
+        
+        // Check if the player is in range and presses the E key to start dialogue
+        if (playerInRange && Input.GetKeyDown(KeyCode.E) && !PlayerMovement.readyToDialgoue)
+        {
+            // Start the dialogue
+            canva.SetActive(true);
+            PlayerMovement.readyToDialgoue = true;
+
+            // Initialize the dialogue system in the NextDialogue script
+            var nextDialogue = canva.GetComponentInChildren<NextDialogue>();
+            if (nextDialogue != null)
+            {
+                nextDialogue.StartDialogue();
+            }
+            Destroy(GameObject.Find("WeaponPickup"));
+            canvaInteract.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Dante_Paraplegic" || other.gameObject.name == "Player" || other.gameObject.name == "Dante_Paraplegic Variant")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInRange = false;
+    }
+}
